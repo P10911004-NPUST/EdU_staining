afa37c23f017d61705a49544304bbdf5bd23f3f9
JK <jklai@gate.sinica.edu.tw>
2026-03-30 17:54:07 +0800

    update

--- pycode/EdU_staining.py
+++ pycode/EdU_staining.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import czifile
 
-img_dir = "C:/jklai/project/rice_heat-stress/experiments/EdU/WT-vs-mutants_Mock_20260128/CZI"
+img_dir = "Z:\\publications\\Osa_heat-stress_2025\\Joon-Keat\\CRP-osrgf1\\geno-compare\\Rep01\\EdU\\img"
 img_list = [i for i in os.listdir(img_dir) if i.endswith((".czi"))]
 
 output_folder = os.path.join(os.path.dirname(img_dir), "OUT_" + os.path.basename(img_dir))
@@ -15,20 +15,22 @@ if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 data = {
-    "img_name": [], 
+    "img_dirname": [], 
+    "img_basename": [], 
     "ntrack": [],
     "Zstacks": [],
     "img_size_pixels": [],
     "img_size_um": [],
     "resolution (um/pixel)": [],
-    "area": [], 
-    "total_intensity": [], 
-    "avg_intensity": [],
+    "EdU_area": [], 
+    "EdU_total_intensity": [], 
+    "EdU_avg_intensity": [],
     "distance_from_root_tip_pixels": [],
     "note": []
 }
 
 for i in img_list:
+    print(f"Processing {os.path.basename(i)}")
     czi = czifile.CziFile(os.path.join(img_dir, i))
     Metadata = czi.metadata(raw=False)["ImageDocument"]["Metadata"]
     ImageScaling = Metadata["ImageScaling"]
@@ -43,25 +45,28 @@ for i in img_list:
     img = czi_arr[0, 0, 0, :, :, :, 0]
     img = np.max(img, axis=0)
     
-    if img.dtype != "uint8":
-        img = (img - img.min()) / (img.max() - img.min()) * 255.0
-        img = img.astype(np.uint8)
+    #if img.dtype != "uint8":
+    #    img = (img - img.min()) / (img.max() - img.min()) * 255.0
+    #    img = img.astype(np.uint8)
     
     img[img < 10] = 0
+    img = (img - img.min()) / (img.max() - img.min()) * 255.0
+    img = img.astype(np.uint8)
 
     total_intensity = img.sum()
     staining_area = np.sum(img > 0)
     avg_intensity = total_intensity / staining_area
 
-    data["img_name"].append(i)
+    data["img_dirname"].append(img_dir)
+    data["img_basename"].append(i)
     data["ntrack"].append(track)
     data["Zstacks"].append(zstack)
     data["img_size_pixels"].append(f'{height} x {width}')
     data["img_size_um"].append(f'{height * microns_per_pixel} x {width * microns_per_pixel}')
     data["resolution (um/pixel)"].append(microns_per_pixel)
-    data["area"].append(staining_area)
-    data["total_intensity"].append(total_intensity)
-    data["avg_intensity"].append(avg_intensity)
+    data["EdU_area"].append(staining_area)
+    data["EdU_total_intensity"].append(total_intensity)
+    data["EdU_avg_intensity"].append(avg_intensity)
     data["distance_from_root_tip_pixels"].append("")
     data["note"].append("")
 
